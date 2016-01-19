a9ace5631e9af9fa532dd4b8a6ca34448318cad3
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-01-19 17:19:23 +0100

    using kvm instead docker

--- server_configure.sh
+++ server_configure.sh
@@ -4,4 +4,4 @@
 echo "#! /bin/bash" >> /home/ubuntu/iperfclient.sh
 echo "screen -d -m -S client iperf -c $server_private -t 300" >> /home/ubuntu/iperfclient.sh
 
-at now +2 min -f /root/iperfclient.sh
+at now +2 min -f /home/ubuntu/iperfclient.sh
