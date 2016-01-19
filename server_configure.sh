6a7c4b795dc48475f585c4b7a56b86ddb8d5c0b3
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-01-19 12:22:01 +0100

    upated path

--- server_configure.sh
+++ server_configure.sh
@@ -2,7 +2,7 @@
 
 echo $new_key > /home/ubuntu/config
 
-echo "#! /bin/bash" >> iperfclient.sh
-echo "screen -d -m -S client iperf -c $server_private -t 300" >> iperfclient.sh
+echo "#! /bin/bash" >> ~/iperfclient.sh
+echo "screen -d -m -S client iperf -c $server_private -t 300" >> ~/iperfclient.sh
 
 at now +2 min -f iperfclient.sh
