f9bf81efe395e3018c423e2ce88be46747b44608
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-01-19 12:13:12 +0100

    trying to schedule

--- server_configure.sh
+++ server_configure.sh
@@ -2,4 +2,7 @@
 
 echo $new_key > /home/ubuntu/config
 
-screen -d -m -S client iperf -c $server_private -t 300
+echo "#! /bin/bash" >> iperfclient.sh
+echo "screen -d -m -S client iperf -c $server_private -t 300" >> iperfclient.sh
+
+at now +2 min -f iperfclient.sh
