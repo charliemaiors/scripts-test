0c6cc5c3e9e7a7cc514b8a93c24c973b1c23c81c
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-01-20 16:26:53 +0100

    Create server-config-docker.sh

---
+++ server-config-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#echo $new_key > /home/ubuntu/config
+touch /root/iperfclient.sh
+echo "#! /bin/bash" >> /root/iperfclient.sh
+echo "screen -d -m -S client iperf -c $server_private -t 300" >> /root/iperfclient.sh
+
+at now +2 min -f /root/iperfclient.sh
