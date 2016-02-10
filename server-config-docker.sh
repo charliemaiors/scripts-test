9346ca0a8ee58776d59337264608bb615e590b8d
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-02-10 15:14:13 +0100

    changed minimum interval

--- server-config-docker.sh
+++ server-config-docker.sh
@@ -5,4 +5,4 @@ touch /root/iperfclient.sh
 echo "#! /bin/bash" >> /root/iperfclient.sh
 echo "screen -d -m -S client iperf -c $server_private -t 300" >> /root/iperfclient.sh
 
-at now +2 min -f /root/iperfclient.sh
+at now +4 min -f /root/iperfclient.sh
