397a02efabb19ff4ee08fc75db2fce1ca6eb13c9
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-02-11 10:31:00 +0100

    updated running time

--- server_configure.sh
+++ server_configure.sh
@@ -2,6 +2,6 @@
 
 #echo $new_key > /home/ubuntu/config
 echo "#! /bin/bash" >> /home/ubuntu/iperfclient.sh
-echo "screen -d -m -S client iperf -c $server_private -t 60" >> /home/ubuntu/iperfclient.sh
+echo "screen -d -m -S client iperf -c $server_private -t 400" >> /home/ubuntu/iperfclient.sh
 
 at now +7 min -f /home/ubuntu/iperfclient.sh
