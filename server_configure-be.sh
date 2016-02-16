80c61034c29ff94835261174c9e923f480d566bc
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-02-16 14:04:35 +0100

    created server configure be

---
+++ server_configure-be.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#echo $new_key > /home/ubuntu/config
+echo "#! /bin/bash" >> /home/ubuntu/iperfclient.sh
+echo "screen -d -m -S client iperf -c $server_private -t 30" >> /home/ubuntu/iperfclient.sh
+
+at now +3 min -f /home/ubuntu/iperfclient.sh
