512cdc1b8f910693870d150a1d956df39df63a2e
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-02-20 13:33:09 +0100

    Update server_configure-be.sh

--- server_configure-be.sh
+++ server_configure-be.sh
@@ -4,4 +4,4 @@
 echo "#! /bin/bash" >> /home/ubuntu/iperfclient.sh
 echo "screen -d -m -S client iperf -c $server_private -t 30" >> /home/ubuntu/iperfclient.sh
 
-at now +3 min -f /home/ubuntu/iperfclient.sh
+at now +5 min -f /home/ubuntu/iperfclient.sh
